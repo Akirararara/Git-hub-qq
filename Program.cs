c10d2b6dde2d6aafd436ac7434800a6735cac214
ワタナベ　アキラ <212E004@GAME.LOCAL>
2021-10-07 17:20:14 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_hub_qq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
